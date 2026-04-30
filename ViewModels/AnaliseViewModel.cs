c9e85e5ae07f0adf3c8a314003d1495a2691641a
RenatoR98 <renator98@outlook.com>
2026-04-30 11:19:05 -0300

    Criação de arquivos para estilos e recursos

--- ViewModels/AnaliseViewModel.cs
+++ ViewModels/AnaliseViewModel.cs
@@ -1,7 +1,25 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Avalonia.Controls.Shapes;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LiveChartsCore.SkiaSharpView.Avalonia;
+using projetoCep.ViewModels.Charts;
 
 namespace projetoCep.ViewModels;
 
 public partial class AnaliseViewModel : ViewModelBase
 {
+    public LineChartViewModel Line { get; } = new();
+
+    public AnaliseViewModel()
+    {
+        AtualizarGraficos();
+    }
+
+    [RelayCommand]
+    private void AtualizarGraficos()
+    {
+        double[] valores = { 10.2, 10.4, 10.1, 10.6, 10.5, 10.3 };
+        Line.Atualizar(valores, valorFixo: 10.5);
+    }
+
 }
---
+++ ViewModels/Charts/LineChartViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System.Linq;
+
+namespace projetoCep.ViewModels.Charts;
+
+public partial class LineChartViewModel : ViewModelBase
+{
+    [ObservableProperty]
+    private ISeries[] series = [];
+
+    [ObservableProperty]
+    private Axis[] xAxes =
+    [
+        new Axis
+        {
+            Name = "Medição",
+            TextSize = 12,
+            NameTextSize = 13
+        }
+    ];
+
+    [ObservableProperty]
+    private Axis[] yAxes =
+    [
+        new Axis
+        {
+            Name = "Valor",
+            TextSize = 12,
+            NameTextSize = 13
+        }
+    ];
+
+    public void Atualizar(double[] valores, double valorFixo)
+    {
+        double[] linhaFixa = valores
+            .Select(_ => valorFixo)
+            .ToArray();
+
+        Series =
+        [
+            new LineSeries<double>
+            {
+                Values = valores,
+                Name = "Valor medido",
+                DataLabelsSize = 12
+            },
+
+            new LineSeries<double>
+            {
+                Values = linhaFixa,
+                Name = "Referência",
+                GeometrySize = 0,
+                DataLabelsSize = 12
+            }
+        ];
+    }
+}
